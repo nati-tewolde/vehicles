c9ba685e4788572f4c3948a36956c853750a1824
nati-tewolde <natisnewemail@gmail.com>
2025-10-27 14:13:03 -0400

    Add Car subclass

---
+++ src/main/java/com/pluralsight/Car.java
@@ -0,0 +1,21 @@
+package com.pluralsight;
+
+public class Car extends Vehicle {
+    private int numberOfDoors;
+
+    public int getNumberOfDoors() {
+        return numberOfDoors;
+    }
+
+    public void setNumberOfDoors(int numberOfDoors) {
+        this.numberOfDoors = numberOfDoors;
+    }
+
+    public void openTrunk() {
+        System.out.println("Opening trunk ...");
+    }
+
+    public void closeTrunk() {
+        System.out.println("Closing trunk ...");
+    }
+}
