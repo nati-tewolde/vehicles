857b887879506bcb7d8b553d95ed79389cb2c858
nati-tewolde <natisnewemail@gmail.com>
2025-10-27 15:16:36 -0400

    Refactor Main class using solution

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -2,20 +2,32 @@ package com.pluralsight;
 
 public class Main {
     public static void main(String[] args) {
-        Moped moped = new Moped();
-        moped.setColor("Red");
-        moped.setFuelCapacity(5);
-
-        Car car = new Car();
-        car.setNumberOfPassengers(7);
-        car.setCargoCapacity(10);
+        Moped slowRide = new Moped();
+        slowRide.setColor("Red");
+        slowRide.setFuelCapacity(5);
 
         SemiTruck semiTruck = new SemiTruck();
-        semiTruck.setTopSpeed(30);
-        semiTruck.accelerate();
+        semiTruck.setNumberOfTrailers(2);
+        semiTruck.setFuelCapacity(100);
 
         HoverCraft hoverCraft = new HoverCraft();
-        hoverCraft.setModel("Tesla");
-        hoverCraft.startEngine();
+        hoverCraft.setAirCushionPressure(50);
+        hoverCraft.setColor("Blue");
+
+        Car car = new Car();
+        car.setNumberOfDoors(4);
+        car.setTopSpeed(200);
+
+        // Test the functionality of the classes and methods
+        slowRide.ride();
+        semiTruck.loadCargo();
+        hoverCraft.hover();
+        car.openTrunk();
+
+        // Accessing Vehicle getters
+        System.out.println("Color of the Moped: " + slowRide.getColor());
+        System.out.println("Fuel capacity of the SemiTruck: " + semiTruck.getFuelCapacity());
+        System.out.println("Air cushion pressure of the HoverCraft: " + hoverCraft.getAirCushionPressure());
+        System.out.println("Top speed of the Car: " + car.getTopSpeed());
     }
 }
